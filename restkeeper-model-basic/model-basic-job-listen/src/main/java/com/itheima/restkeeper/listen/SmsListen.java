bcfa39e5ebdcfd6e14b23ec7eca81e26b79daff6
itheima <2998429686@qq.com>
2022-02-08 23:46:44 +0800

    完成品牌管理CRUD，自测完成

--- restkeeper-model-basic/model-basic-job-listen/src/main/java/com/itheima/restkeeper/listen/SmsListen.java
+++ restkeeper-model-basic/model-basic-job-listen/src/main/java/com/itheima/restkeeper/listen/SmsListen.java
@@ -2,14 +2,9 @@ package com.itheima.restkeeper.listen;
 
 import com.alibaba.fastjson.JSONObject;
 import com.itheima.restkeeper.adapter.SmsSendAdapter;
-import com.itheima.restkeeper.pojo.LogBusiness;
 import com.itheima.restkeeper.pojo.MqMessage;
-import com.itheima.restkeeper.req.LogBusinessVo;
 import com.itheima.restkeeper.req.SendMessageVo;
-import com.itheima.restkeeper.service.ILogBusinessService;
-import com.itheima.restkeeper.sink.LogSink;
 import com.itheima.restkeeper.sink.SmsSink;
-import com.itheima.restkeeper.utils.BeanConv;
 import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.support.AmqpHeaders;
@@ -36,7 +31,7 @@ public class SmsListen {
     public void onMessage(@Payload MqMessage message,
                           @Header(AmqpHeaders.CHANNEL) Channel channel,
                           @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) throws IOException {
-        String jsonConten = message.getConten();
+        String jsonConten = message.getContent();
         log.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
         SendMessageVo sendMessageVo = JSONObject.parseObject(jsonConten, SendMessageVo.class);
         boolean flag = smsSendAdapter.SendSms(
